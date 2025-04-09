ea76cc155f06b226fc0869cad761da65aa4d7316
Nate Cole <natercole@outlook.com>
2025-04-09 16:03:48 -0400

    cta image fix

--- src/components/ui/CTASection.tsx
+++ src/components/ui/CTASection.tsx
@@ -22,9 +22,9 @@ const CTASection = () => {
       <Image
         src="/assets/podcastPicSix.svg"
         alt="Woman listening to podcast"
-        width="0"
-        height="0"
-        style={{ width: "100%", height: "100%" }}
+        width={100}
+        height={100}
+        className="w-full h-full object-cover"
       />
     </div>
   );
